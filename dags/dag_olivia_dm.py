13ac44c89141f05190e2e05a4955716ac2e3b5b8
Eric Janssens <eric.janssens@fooinc.be>
2026-04-03 14:21:17 +0200

    testing for simpelest output

--- dags/dag_olivia_dm.py
+++ dags/dag_olivia_dm.py
@@ -3,7 +3,7 @@ import logging
 from datetime import datetime, timedelta
 from airflow import DAG
 # from airflow.sensors.python import PythonSensor
-# from airflow.operators.python import PythonOperator
+from airflow.operators.python import PythonOperator
 # from airflow.operators.bash import BashOperator
 
 # from airflow.providers.microsoft.fabric.operators.run_item import MSFabricRunJobOperator
@@ -27,7 +27,9 @@ from plugins.fabric_run_pipeline import fabric_run_pipeline
 # DV connection
 ENV = 'dv' # development
 # FABRIC_CONN_ID = "demo-fabric-tenant"
-FABRIC_CONN_ID = Variable.get(f"{ENV}_envar_fabric_conn_id")
+
+# UNCOMMENT AFTER TESTING
+# FABRIC_CONN_ID = Variable.get(f"{ENV}_envar_fabric_conn_id")
 # QA connection
 # FABRIC_CONN_ID = "fabric-tenant-QA"
 
@@ -35,19 +37,27 @@ FABRIC_CONN_ID = Variable.get(f"{ENV}_envar_fabric_conn_id")
 # Configuration - polling system details
 # =============================================================================
 # WS_Technical_DV.DB-Technical
-SQL_SERVER = Variable.get(f"{ENV}_sqlserver_dbtechnical")
-SQL_DATABASE = Variable.get(f"{ENV}_sqlserver_dbtechnical_database")
-TABLE_NAME = Variable.get(f"{ENV}_sqlserver_dbtechnical_databasetable")
+
+# UNCOMMENT AFTER TESTING
+# SQL_SERVER = Variable.get(f"{ENV}_sqlserver_dbtechnical")
+# SQL_DATABASE = Variable.get(f"{ENV}_sqlserver_dbtechnical_database")
+# TABLE_NAME = Variable.get(f"{ENV}_sqlserver_dbtechnical_databasetable")
 
 # =============================================================================
 # Configuration - Fabric Pipeline details
 # =============================================================================
 # WS_Analytical_Raw_DV
-WORKSPACE_ID = Variable.get(f"{ENV}_airflow_workspace_id")
+
+# UNCOMMENT AFTER TESTING
+# WORKSPACE_ID = Variable.get(f"{ENV}_airflow_workspace_id")
 # WS_Analytical_Raw_DV.Source.SALESFORCE.PL_Load_SALESFORCE
-PL_Load_SALESFORCE_ID = Variable.get(f"{ENV}_pipeline_Salesforce_id")
+
+# UNCOMMENT AFTER TESTING
+# PL_Load_SALESFORCE_ID = Variable.get(f"{ENV}_pipeline_Salesforce_id")
 # WS_Analytical_Raw_DV.Source.OLIVIA.PL_Load_OLIVIA
-PL_Load_OLIVIA_ID = Variable.get(f"{ENV}_pipeline_Olivia_id")
+
+# UNCOMMENT AFTER TESTING
+# PL_Load_OLIVIA_ID = Variable.get(f"{ENV}_pipeline_Olivia_id")
 # WS_Analytical_Raw_QA
 # WORKSPACE_ID = Variable.get("qa_airflow_workspace_id")
 # WS_Analytical_Raw_QA.Source.SALESFORCE.PL_Load_SALESFORCE
@@ -57,6 +67,9 @@ PL_Load_OLIVIA_ID = Variable.get(f"{ENV}_pipeline_Olivia_id")
 
 logger = logging.getLogger("airflow.task")
 logger.info(f"/nUse deferred for this task: xxxxxxx/n")
+
+def hello_world():
+    logger.info("Hello World, this is output from Fabric Managed Airflow!")
 
 # =============================================================================
 # DAG Definition
@@ -77,19 +90,27 @@ with DAG(
     # on_failure_callback=failure_callback,
     tags=['fabric', 'dbt', ENV]
 ) as dag:
+    # ===================================================
+    # BRANCH TEST
+    # ===================================================
+
+    do_something = PythoOperator(
+        task_id = "Hello there",
+        python_callable = hello_world
+    )
 
     # ===================================================
     # BRANCH SENSOR
     # ===================================================
 
-    wait_for_files = run_python_sensor(
-        task_id="wait_for_sql_data",
-        conn_id=FABRIC_CONN_ID,
-        sql_server=SQL_SERVER,
-        database=SQL_DATABASE,
-        table_name=TABLE_NAME,
-        file_count_limit=2
-    )
+    # wait_for_files = run_python_sensor(
+        # task_id="wait_for_sql_data",
+        # conn_id=FABRIC_CONN_ID,
+        # sql_server=SQL_SERVER,
+        # database=SQL_DATABASE,
+        # table_name=TABLE_NAME,
+        # file_count_limit=2
+    # )
     
     # ===================================================
     # BRANCH SALESFORCE
@@ -125,8 +146,9 @@ with DAG(
     # BRANCH dbt refresh
     # ===================================================
 
-    DBT_ACCOUNT_ID = Variable.get("dbt_account_id") ## unused - to be removed
-    DBT_OLIVIA_DM_JOB_ID = Variable.get("dbt_Olivia_DM_jobid")
+    # UNCOMMENT AFTER TESTING
+    # DBT_ACCOUNT_ID = Variable.get("dbt_account_id") ## unused - to be removed
+    # DBT_OLIVIA_DM_JOB_ID = Variable.get("dbt_Olivia_DM_jobid")
 
     # dbt_job =  dbt_run_job(
     #     task_id="run_olivia_dm",
@@ -164,5 +186,5 @@ with DAG(
     # PARALLEL EXECUTION
     # ===================================================
 
-    wait_for_files# >> [run_pipeline_SALESFORCE, run_pipeline_OLIVIA]# >> dbt_job_run
+    # wait_for_files# >> [run_pipeline_SALESFORCE, run_pipeline_OLIVIA]# >> dbt_job_run
     
