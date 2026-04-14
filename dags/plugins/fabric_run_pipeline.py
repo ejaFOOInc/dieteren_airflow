fb416cc18c57c5335e514ae32e48dfda3f535e7a
Eric Janssens <eric.janssens@fooinc.be>
2026-04-14 14:59:46 +0200

    Updated parameter set for fabric_run_pipeline, disabled parameter ENABLE

--- dags/plugins/fabric_run_pipeline.py
+++ dags/plugins/fabric_run_pipeline.py
@@ -11,8 +11,8 @@ def fabric_run_pipeline(
         timeout :int,
         deferrable :bool,
         parm_SourceName :str,
-        parm_ApplicationName :str,
-        enable :bool
+        parm_ApplicationName :str#,
+        # enable :bool
 ):
     
     logger = logging.getLogger("airflow.task")
